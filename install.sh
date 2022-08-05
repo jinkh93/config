0c7a2d339ae4caa65e5cca8b8ced393305147db8
jk <jinkh93@gmail.com>
2022-08-05 11:38:30 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 cp .vimrc .vimrc.plug .zshrc .zprofile ~ \
+    && source ~/.zprofile && source ~/.zshrc \
     && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval $(/opt/homebrew/bin/brew shellenv) \
     && brew install nvm && nvm install node \
     && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
